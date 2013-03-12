6f7ea0375453230b51bee510f5631d42505efd18
Chuka Okoye <okoye9@gmail.com>
2013-03-12 10:25:57 -0700

    working on stats

---
+++ stats2.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+from scipy import stats
+
+def parse(line, majo, mino):
+   key, value = line.split(majo)
+   proc, req = key.split(mino)
+   return (int(proc), int(req), float(value))
+
+def main(input_file):
+   readings = []
+   for line in open(input_file):
+      readings.append(parse(line, '\t', ':'))
+
+   #now, compute 90th percentile value
+   l = lambda z: z[2]
+   p = stats.scoreatpercentile(map(l, readings), 99)
+   print p
+      
+
+
+if __name__ == '__main__':
+   main('../results/19NodeRun.log')
